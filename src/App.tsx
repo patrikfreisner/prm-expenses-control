b154b0635908dc0ddc6a52282497c7705bdb659e
Patrik Reisner <patrikfr.nx@gmail.com>
2022-05-10 16:47:46 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { ApplicationRouterConfig } from './Component/RouterGuardComponent';
 import { LoginProvider } from './Context/LoginContext';
+import { getItem } from './Services/InvokeAWS/InvokeBaseDynamoDBAPI';
 
 const App = () => {
   return (
