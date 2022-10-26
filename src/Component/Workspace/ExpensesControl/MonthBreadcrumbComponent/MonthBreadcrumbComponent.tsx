be455f854273498c159e076a328b1dbb8413fb20
Patrik Reisner <patrikfr.nx@gmail.com>
2022-10-25 21:11:28 -0300

    ...

--- src/Component/Workspace/ExpensesControl/MonthBreadcrumbComponent/MonthBreadcrumbComponent.tsx
+++ src/Component/Workspace/ExpensesControl/MonthBreadcrumbComponent/MonthBreadcrumbComponent.tsx
@@ -1,18 +1,22 @@
-import { Box, Breadcrumbs, Typography, Link, Grid, Divider, Avatar } from '@mui/material'
+import { Box, Breadcrumbs, Typography, Link, Grid, Divider, Avatar, Backdrop, Dialog, DialogTitle, DialogContent, IconButton } from '@mui/material'
 import React, { useEffect, useState } from 'react'
-import ExpenseSpeedDialComponent from '../ExpenseSpeedDialComponent/ExpenseSpeedDialComponent'
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import CloseIcon from '@mui/icons-material/Close';
 
 import './MonthBreadcrumbStyle.css'
 import { deepOrange, blue, grey, green } from '@mui/material/colors';
 import { ExpenseDateTime, Month } from '../../../../Class/ExpenseClasses';
 import { useExpensesContext } from '../../../../Context/ExpensesContext';
+import { MonthInsertUpdateComponent } from '../MonthInsertUpdateComponent/MonthInsertUpdateComponent';
 
 export const MonthBreadcrumbComponent = () => {
 
     const { monthValues, currentMonth, setCurrentMonth } = useExpensesContext();
     const [month, setMonth] = useState(new Array<Month>());
     const [currentTabIndex, setcurrentTabIndex] = useState(Number);
+    const [showBackdrop, setShowBackdrop] = useState(false);
+    const [showMonthDialog, setShowMonthDialog] = useState(false);
 
     useEffect(() => {
         if (currentMonth.getDateObject().getMonth() === 0) {
@@ -38,10 +42,10 @@ export const MonthBreadcrumbComponent = () => {
             let _month = new ExpenseDateTime(currentMonth.getDateObject());
             _month.setMonth(_month.getMonth() - -index);
 
-            // if (_month.getFullYear() == currentMonth.getDateObject().getFullYear()) {
-            let _monthObj: Month = new Month({ sk: "MONTH#" + _month.toFilterString() });
-            createdMonthList.push(_monthObj);
-            // }
+            if (_month.getFullYear() == currentMonth.getDateObject().getFullYear()) {
+                let _monthObj: Month = new Month({ sk: "MONTH#" + _month.toFilterString() });
+                createdMonthList.push(_monthObj);
+            }
         }
 
         if (!currentTabIndex) setcurrentTabIndex(createdMonthList.length);
@@ -53,9 +57,9 @@ export const MonthBreadcrumbComponent = () => {
             _month.setMonth(_month.getMonth() + index);
 
             let _monthObj: Month = new Month({ sk: "MONTH#" + _month.toFilterString() });
-            // if (_month.getFullYear() == currentMonth.getDateObject().getFullYear()) {
-            createdMonthList.push(_monthObj);
-            // }
+            if (_month.getFullYear() == currentMonth.getDateObject().getFullYear()) {
+                createdMonthList.push(_monthObj);
+            }
         }
 
         createdMonthList = createdMonthList.map((item: Month) => {
@@ -83,14 +87,54 @@ export const MonthBreadcrumbComponent = () => {
         }
     }
 
+    const handleCreateMonth = () => {
+        handleMonthDialogOpen();
+    };
+
+    const handleMonthDialogOpen = () => setShowMonthDialog(true);
+    const handleMonthDialogClose = () => { setShowMonthDialog(false); setShowBackdrop(false); };
+
     return (
         <Grid className='outline-box'>
-            <Grid item xs={12}>
+            <Backdrop open={showBackdrop} />
+            <Grid item xs={12} style={{ display: "flex", justifyContent: 'space-evenly', alignItems: 'center' }}>
+                {currentMonth.getDateObject().getMonth() == 0 &&
+                    <>
+                        <Avatar key={currentMonth.pk + currentMonth.sk + "0"}
+                            sx={{ bgcolor: blue[700], flexGrow: 1 }}
+                            onClick={() => {
+                                var _month = currentMonth.getDateObject();
+                                _month.setMonth(currentMonth.getDateObject().getMonth() - 1);
+                                handleChangeMonth(new Month({ sk: "MONTH#" + _month.toFilterString() }), 0)
+                            }}
+                            variant="square">
+                            {currentMonth.getDateObject().getFullYear() - 1}
+                        </Avatar>
+                        <ArrowBackIosNewIcon />
+                    </>
+                }
+
                 <Avatar key={currentMonth.pk + currentMonth.sk}
-                    sx={{ bgcolor: blue[700], width: "100%" }}
+                    sx={{ bgcolor: blue[700], flexGrow: 1 }}
                     variant="square">
                     {currentMonth.getDateObject().getFullYear()}
                 </Avatar>
+
+                {currentMonth.getDateObject().getMonth() == 11 &&
+                    <>
+                        <ArrowForwardIosIcon />
+                        <Avatar key={currentMonth.pk + currentMonth.sk + "1"}
+                            sx={{ bgcolor: blue[700], flexGrow: 1 }}
+                            onClick={() => {
+                                var _month = currentMonth.getDateObject();
+                                _month.setMonth(currentMonth.getDateObject().getMonth() + 1);
+                                handleChangeMonth(new Month({ sk: "MONTH#" + _month.toFilterString() }), 0)
+                            }}
+                            variant="square">
+                            {currentMonth.getDateObject().getFullYear() + 1}
+                        </Avatar>
+                    </>
+                }
             </Grid>
             <Grid className='breadcrumb-box' item xs={12}>
                 <Breadcrumbs
@@ -103,13 +147,41 @@ export const MonthBreadcrumbComponent = () => {
                             <Avatar key={item.pk + item.sk}
                                 sx={{ bgcolor: handleSquareColor(item) }}
                                 variant="square"
-                                onClick={() => handleChangeMonth(item, index)}>
+                                onClick={() => {
+                                    if (item.hasMonthCreated()) {
+                                        handleChangeMonth(item, index);
+                                    } else {
+                                        handleCreateMonth();
+                                    }
+                                }}>
                                 {item.getDateObject().getFormatedMonth()}
                             </Avatar>
                         );
                     })}
                 </Breadcrumbs>
             </Grid>
+            <Dialog
+                open={showMonthDialog}
+                onClose={handleMonthDialogClose}
+            >
+                <DialogTitle align='left'>
+                    Novo mês
+                    <IconButton
+                        aria-label="close"
+                        onClick={handleMonthDialogClose}
+                        sx={{
+                            position: 'absolute',
+                            right: 16,
+                            top: 16,
+                        }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                </DialogTitle>
+                <DialogContent>
+                    <MonthInsertUpdateComponent onSuccess={handleMonthDialogClose} onFailed={handleMonthDialogClose} onCancel={handleMonthDialogClose} />
+                </DialogContent>
+            </Dialog>
         </Grid>
     )
 }
